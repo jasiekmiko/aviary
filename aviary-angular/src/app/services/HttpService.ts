fc4df7c7c68ed3ba0589204c2d7d4a9d183e7264
Jan Mikolajczak <jan@bluehawkqs.eu>
2017-09-16 11:51:14 +0100

    Fix Http service

--- aviary-angular/src/app/services/HttpService.ts
+++ aviary-angular/src/app/services/HttpService.ts
@@ -1,11 +1,11 @@
 import {Inject} from '@angular/core';
 import {
-    Http,
-    XHRBackend,
-    RequestOptions,
-    RequestOptionsArgs,
-    Response,
-    Headers
+  Http,
+  XHRBackend,
+  RequestOptions,
+  RequestOptionsArgs,
+  Response,
+  Headers
 } from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
@@ -13,35 +13,37 @@ import 'rxjs/add/operator/catch';
 import {AngularFireAuth} from "angularfire2/auth";
 
 export class HttpService extends Http {
-    constructor(@Inject(XHRBackend) backend: XHRBackend,
-                @Inject(RequestOptions) options: RequestOptions,
-                @Inject(AngularFireAuth) private auth: AngularFireAuth) {
-        super(backend, options);
-    }
+  constructor(@Inject(XHRBackend) backend: XHRBackend,
+              @Inject(RequestOptions) options: RequestOptions,
+              @Inject(AngularFireAuth) private auth: AngularFireAuth) {
+    super(backend, options);
+  }
 
-    get(route: string, options?: RequestOptionsArgs): Observable<Response> {
-      const url = "http://localhost:8880/" + route;
-        return this.auth
-                .idToken
-                .flatMap(token => {
-                    if (!options) {
-                        options = {headers: new Headers()};
-                    }
-                    options.headers.set('Authorization', `Bearer ${token}`);
-                    let response = super.get(url, options);
-                    response.catch(this.catchAuthError());
-                    return response
-                });
-    }
-
-    private catchAuthError() {
-        return (res: Response) => {
-            console.log(res);
-            if (res.status === 401 || res.status === 403) {
-                // if not authenticated
-                console.log(res);
+  get(route: string, options?: RequestOptionsArgs): Observable<Response> {
+    const url = "http://localhost:8880/" + route;
+    return this.auth
+      .authState.flatMap(user => {
+        return Observable.fromPromise(user.getToken() as Promise<string>)
+          .flatMap(token => {
+            if (!options) {
+              options = {headers: new Headers()};
             }
-            return Observable.throw(res);
-        };
+            options.headers.set('Authorization', `Bearer ${token}`);
+            let response = super.get(url, options);
+            response.catch(this.catchAuthError);
+            return response;
+          })
+      })
+
+  }
+
+  private catchAuthError = (res: Response) => {
+    console.log(res);
+    if (res.status === 401 || res.status === 403) {
+      // if not authenticated
+      console.log(res);
     }
+    return Observable.throw(res);
+  };
+
 }
