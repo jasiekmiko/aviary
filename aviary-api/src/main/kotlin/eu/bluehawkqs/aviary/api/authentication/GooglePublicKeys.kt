64d727b20a6e50518f9887b21cdbd9c95e91848f
Jan Mikolajczak <jan@bluehawkqs.eu>
2017-12-02 13:55:00 +0000

    Rewrite GooglePublicKeys class to be more idiomatic and simpler

--- aviary-api/src/main/kotlin/eu/bluehawkqs/aviary/api/authentication/GooglePublicKeys.kt
+++ aviary-api/src/main/kotlin/eu/bluehawkqs/aviary/api/authentication/GooglePublicKeys.kt
@@ -7,38 +7,27 @@ import java.net.URL
 
 class GooglePublicKeys private constructor() {
 
-    private var timestamp: Long = 0
-
-    private var keys: Map<String, String>? = null
+    private var keys = fetchGooglePublicKeys()
+    private var timestamp = System.currentTimeMillis()
 
     companion object {
-
-        private val GOOGLE_PUBLIC_KEYS = "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com"
-
-        private val ONE_DAY = 1000 * 60 * 60 * 24
-
-        private var instance: GooglePublicKeys? = null
+        const private val GOOGLE_PUBLIC_KEYS = "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com"
+        const private val ONE_DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24
 
         fun getKeys(): Map<String, String> {
-            if (instance == null || isOutdated) {
-                reload()
-            }
-
-            return instance!!.keys!!
+            return instance.keys
         }
 
-        private val isOutdated: Boolean
-            get() = instance!!.timestamp < System.currentTimeMillis() - ONE_DAY
+        private val minimumFreshness
+            get() = System.currentTimeMillis() - ONE_DAY_IN_MILLISECONDS
 
-        @Synchronized private fun reload() {
-            if (instance != null && !isOutdated) {
-                return
+        private var instance = GooglePublicKeys()
+            get() {
+                if (field.timestamp < minimumFreshness) {
+                    field = GooglePublicKeys()
+                }
+                return field
             }
-
-            instance = GooglePublicKeys()
-            instance!!.keys = fetchGooglePublicKeys()
-            instance!!.timestamp = System.currentTimeMillis()
-        }
 
         private fun fetchGooglePublicKeys(): Map<String, String> {
             return parseJson(fetchKeysJson())
@@ -47,13 +36,11 @@ class GooglePublicKeys private constructor() {
         private fun fetchKeysJson(): String {
             try {
                 val urlFetch = URLFetchServiceFactory.getURLFetchService()
-
                 val response = urlFetch.fetch(URL(GOOGLE_PUBLIC_KEYS))
-                return String(response.getContent())
+                return String(response.content)
             } catch (e: IOException) {
                 throw RuntimeException(e)
             }
-
         }
 
         private fun parseJson(json: String): Map<String, String> {
